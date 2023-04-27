1b70bfcb3b1a0b4baa31c7b1b6b9cfff2a9dcdb7
Yurii Rybachok <drummer2009@ukr.net>
2023-04-27 14:32:10 +0300

    create TextInput component

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,31 @@
 <template>
     <AuthLayout>
+        <div class="mt-10">
+            <h1 class="lg:text-5xl text-3xl text-center font-extrabold">
+                Log in to your Linktree
+            </h1>
 
+            <form
+                class="mt-12"
+                @submit.prevent="login()"
+            >
+                <div>
+                    <TextInput 
+                        placeholder="Email: link@gmail.com"
+                        v-model:input="email"
+                        inputType="email"
+                        :error="errors && errors.email ? errors.email[0] : ''"
+                    />
+                </div>
+            </form>
+        </div>
     </AuthLayout>
 </template>
 
 <script setup>
 import AuthLayout from '~/layouts/AuthLayout.vue';
+
+let email = ref(null)
+let password = ref(null)
+let errors = ref(null)
 </script>
