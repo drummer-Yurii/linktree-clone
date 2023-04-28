b106f06bf9fcd95c81add2dc3698079d8315f47a
Yurii Rybachok <drummer2009@ukr.net>
2023-04-28 12:13:50 +0300

    AdminLayout setup

---
+++ pages/admin/index.vue
@@ -0,0 +1,14 @@
+<template>
+    <AdminLayout>
+        <div
+            id="AdminPage"
+            class="flex h-[100vh] pb-4"
+        >
+            AdminPage
+        </div>
+    </AdminLayout>
+</template>
+
+<script setup>
+import AdminLayout from '~/layouts/AdminLayout.vue';
+</script>
